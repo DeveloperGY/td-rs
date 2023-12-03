a0c92ba6f6e868553e8fd12b752fe900af6daf82
DeveloperGY <dev.garrettyenojr@gmail.com>
2023-12-03 01:09:00 -0500

    added color support, hopefully

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
 use std::io::Write;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Color (u8, u8, u8);
+
 pub struct Terminal {
     width: usize,
     height: usize,
 
-    buf: Box<[Box<[char]>]>
+    ch: Box<[Box<[char]>]>,
+    fg: Box<[Box<[Color]>]>,
+    bg: Box<[Box<[Color]>]>
 }
 
 impl Terminal {
@@ -15,43 +20,79 @@ impl Terminal {
         let columns = win_size.ws_col as usize;
         let rows = win_size.ws_row as usize;
 
-        let mut row = Vec::new();
-        row.resize(columns, ' ');
 
-        let mut arr = Vec::new();
-        arr.resize(rows, row.clone().into_boxed_slice());
+        let mut ch_row = Vec::new();
+        ch_row.resize(columns, ' ');
+
+        let mut ch_buf = Vec::new();
+        ch_buf.resize(rows, ch_row.clone().into_boxed_slice());
+
+
+        let mut color_row = Vec::new();
+        color_row.resize(columns, Color(0, 0, 0));
+
+        let mut fg_buf = Vec::new();
+        fg_buf.resize(rows, color_row.clone().into_boxed_slice());
+
+        let mut bg_buf = Vec::new();
+        bg_buf.resize(rows, color_row.clone().into_boxed_slice());
+
 
         Self {
             width: columns,
             height: rows,
-            buf: arr.into_boxed_slice()
+            ch: ch_buf.into_boxed_slice(),
+            fg: fg_buf.into_boxed_slice(),
+            bg: bg_buf.into_boxed_slice()
         }
     }
 
-    pub fn set_pixel(&mut self, x: usize, y: usize, ch: char) {
+    pub fn set_char(&mut self, x: usize, y: usize, ch: char) {
         if !self.is_valid_coords(x, y) {return};
-        self.buf[x][y] = ch;
+        self.ch[y][x] = ch;
+    }
+
+    pub fn set_color_char(&mut self, x: usize, y: usize, ch: char, fg: Color, bg: Color) {
+        if !self.is_valid_coords(x, y) {return};
+        self.ch[y][x] = ch;
+        self.fg[y][x] = fg;
+        self.bg[y][x] = bg;
+    }
+
+    pub fn set_fg(&mut self, x: usize, y: usize, color: Color) {
+        if !self.is_valid_coords(x, y) {return};
+        self.fg[y][x] = color;
+    }
+
+    pub fn set_bg(&mut self, x: usize, y: usize, color: Color) {
+        if !self.is_valid_coords(x, y) {return};
+        self.bg[y][x] = color;
     }
 
     pub fn clear(&mut self) {
-        self.buf.iter_mut().for_each(|line| {
-            line.iter_mut().for_each(|ch| {
-                *ch = ' ';
-            })
-        });
+        for y in 0..self.height {
+            for x in 0..self.width {
+                self.ch[y][x] = ' ';
+                self.fg[y][x] = Color(0, 0, 0);
+                self.bg[y][x] = Color(0, 0, 0);
+            }
+        }
     }
 
     pub fn display(&self) {
-        self.buf.iter().enumerate().for_each(|(index, chars)| {
-            
+        let mut output = String::new();
 
-            if index < self.height-1 {
-                print!("{}\n", chars.iter().collect::<String>());
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let fg = self.fg[y][x];
+                let bg = self.bg[y][x];
+
+                output += format!("\x1b[38;2;{};{};{}m\x1b[48;2;{};{};{}m{}", fg.0, fg.1, fg.2, bg.0, bg.1, bg.2, self.ch[y][x]).as_str();
             }
-            else {
-                print!("{}", chars.iter().collect::<String>());
-            }
-        });
+            output += "\n"
+        }
+
+        print!("{}\x1b[m", output.trim_end());
 
         let _ = std::io::stdout().flush();
     }
